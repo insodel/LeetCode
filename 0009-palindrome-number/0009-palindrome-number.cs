28395cdd9e44bb509b1ca1507a15deed1e890604
Aitor <86533955+insodel@users.noreply.github.com>
2023-09-14 13:04:54 +0200

    Time: 40 ms (58.27%), Space: 30 MB (55.7%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        string str = x.ToString();
+        int left = 0;
+        int right = str.Length - 1;
+
+        while (left < right)
+        {
+          if (str[left] != str[right]) 
+          {
+              return false;
+          }
+          left++;
+          right--;
+        }
+        return true;
+    }
+}
